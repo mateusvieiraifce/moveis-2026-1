92073c5ad793817cdc54d10b4ef25d4d090c6cbe
mateusvieiraifce <101875553+mateusvieiraifce@users.noreply.github.com>
2026-03-02 14:42:45 -0300

    subindo aula02
    subindo aula02

---
+++ Aula02/Aula02/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.branco}>Ola mundo</Text>
+      <Text style={styles.branco}>Seja bem vindo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  branco:{
+    color:'#fff',
+    fontWeight:'bold',
+    fontSize:20,
+  }
+
+});
